369f8517756c7371ed6db7d44cbefae73819b6b7
shivamvarshneyy <shivamvarshney1506@gmail.com>
2025-02-17 21:02:29 +0530

    error_page updated

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Error = () => {
   return (
     <>
-        <div className="error">
-        <h1>Oops!!! <span style={{color : "#FF0000"}}> Error 404 </span>, Page not Found <span className='animated'>😢</span></h1>
-        </div>
+      <div className="error-container">
+        <h1>
+          Oops!!! <span className="error-text">Error 404</span>, Page Not Found 
+          <span className="animated"> 😢</span>
+        </h1>
+        <p className="error-message">The page you're looking for doesn't exist or has been moved.</p>
+        <NavLink to="/" className="btn btn-outline-dark error-btn">Go Back Home</NavLink>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Error;
